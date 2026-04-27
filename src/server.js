76218391348f51f2d0e55d2a0c5cb6d8430f6122
divya_assig10 <dbonthal@charlotte.edu>
2026-04-27 12:09:28 -0400

    modified seed and correct health endpoint

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 if (process.env.NODE_ENV !== 'test') app.use(morgan('tiny'));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs)); 
-app.get('/health', (req, res) => {
+app.get('/api/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });             
 app.use('/api/products', productsRoutes);
